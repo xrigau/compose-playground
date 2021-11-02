bdaa4a5ebf23eb44bd10361399087416220517db
Xavi Rigau <xrigau@gmail.com>
2021-11-02 20:22:14 +0100

    Fix dragging to only animate when the drag ends

--- app/src/main/java/com/xrigau/composeplayground/GesturesActivity.kt
+++ app/src/main/java/com/xrigau/composeplayground/GesturesActivity.kt
@@ -3,20 +3,25 @@ package com.xrigau.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.VectorConverter
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.consumeAllChanges
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.xrigau.composeplayground.ui.theme.ComposeplaygroundTheme
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
 class GesturesActivity : ComponentActivity() {
@@ -36,24 +41,34 @@ class GesturesActivity : ComponentActivity() {
 @Composable
 fun DraggableContent() {
     Box(modifier = Modifier.fillMaxSize()) {
-        var offsetX by remember { mutableStateOf(0f) }
-        var offsetY by remember { mutableStateOf(0f) }
-        val offsetYAnim: Float by animateFloatAsState(offsetY)
+        val offset = remember { Animatable(Offset.Zero, Offset.VectorConverter) }
 
         Box(
             Modifier
-                .offset { IntOffset(offsetX.roundToInt(), offsetYAnim.roundToInt()) }
+                .offset { offset.value.toIntOffset() }
                 .background(Color.Gray)
                 .height(50.dp)
                 .fillMaxWidth()
                 .pointerInput(Unit) {
-                    detectDragGestures(onDrag = { change, dragAmount ->
-                        change.consumeAllChanges()
-//                        offsetX += dragAmount.x
-                        offsetY += dragAmount.y
-                    },
-                        onDragEnd = { offsetY = 0f })
+                    coroutineScope {
+                        detectDragGestures(
+                            onDrag = { change, dragAmount ->
+                                change.consumeAllChanges()
+
+                                launch {
+                                    offset.snapTo(offset.value + Offset(0f, dragAmount.y))
+                                }
+                            },
+                            onDragEnd = {
+                                launch {
+                                    offset.animateTo(Offset.Zero)
+                                }
+                            }
+                        )
+                    }
                 }
         )
     }
 }
+
+private fun Offset.toIntOffset() = IntOffset(x.roundToInt(), y.roundToInt())
